70f0503be053458bf696a0151809b8c355f15f30
Sifat <sifat0666@gmail.com>
2022-11-15 21:52:10 +0600

    config tweak

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar'
-import Sidebar from '../components/Sidebar'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
-  const [isSSR, setIsSSR] = useState(true)
-  
-  useEffect(()=> {
-    setIsSSR(false)
-  }, [])
-  
-  if(isSSR) return null
+  const [isSSR, setIsSSR] = useState(true);
 
-  return(
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if (isSSR) return null;
+
+  return (
     <div>
-      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN!}>
-      <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
-      <Navbar />
-      <div className="flex gap-6 md:gap-20">
-        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-          <Sidebar />
+      {/* <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN!}>
+        <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
+          <Navbar />
+          <div className="flex gap-6 md:gap-20">
+            <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+              <Sidebar />
+            </div>
+            <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+              <Component {...pageProps} />
+            </div>
+          </div>
         </div>
-        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
-          <Component {...pageProps} />
-        </div>
-      </div>
-      </div>
- 
+      </GoogleOAuthProvider> */}
 
-      </GoogleOAuthProvider>
-   </div>
-  ) 
-
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
